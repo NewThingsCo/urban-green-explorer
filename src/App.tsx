31ad6bf6a794e6df1a9fe23f97d41e693f81205b
Niko Pettersen <niko.pettersen@newthings.co>
2021-11-17 14:46:37 +0200

    Convert existing templates to JSX

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { defineComponent } from 'vue';
+import './App.css';
+import Logo from './assets/logo.png';
+import HelloWorld from './components/HelloWorld';
+
+export default defineComponent({
+  name: 'App',
+  render() {
+    return (
+      <>
+        <img alt="Vue logo" src={Logo} />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </>
+    );
+  },
+});
