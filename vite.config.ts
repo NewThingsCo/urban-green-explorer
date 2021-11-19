8d44f5302d7e305e7b3f51efc04b2d09bdda9471
Niko Pettersen <niko.pettersen@newthings.co>
2021-11-19 09:30:50 +0200

    Add Node Version Manager (#14)
    * Add `.nvmrc` to enforce Node version
    
    * Add engines to `package.json`

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/account/',
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
@@ -17,5 +18,4 @@ export default defineConfig({
   server: {
     port: 8080,
   },
-  base: '/account/',
 });
