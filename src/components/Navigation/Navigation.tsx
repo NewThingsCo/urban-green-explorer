9140cc7dcc7c04a1058e8979fa9aa9786e7ece2d
Sarah <robertz.sarah@gmail.com>
2022-04-25 20:26:01 +0300

    Fix: Minor facelift for header and footer - Added icons and minor style improvements

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,6 +2,11 @@ import type { VNode } from 'vue';
 import { defineComponent } from 'vue';
 import './Navigation.css';
 import { RouterLink } from 'vue-router';
+import {
+  GlobeAltIcon,
+  InformationCircleIcon,
+  HomeIcon,
+} from '@heroicons/vue/solid';
 import { routes } from '@/router';
 
 export default defineComponent({
@@ -11,7 +16,9 @@ export default defineComponent({
       <nav class="navigation">
         {routes.map((route) => (
           <RouterLink class="router-link" to={route.path}>
-            {route.name}
+            {'home' === route.name && <HomeIcon class="icon" />}
+            {'about' === route.name && <InformationCircleIcon class="icon" />}
+            {'map' === route.name && <GlobeAltIcon class="icon" />}
           </RouterLink>
         ))}
       </nav>
