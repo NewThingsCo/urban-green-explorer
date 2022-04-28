f77c9569f5acc644c60dba6303cb70b7c0c86509
Sarah <robertz.sarah@gmail.com>
2022-04-28 20:26:09 +0300

    Fix: Minor modifications on header and footer

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,11 +2,10 @@ import type { VNode } from 'vue';
 import { defineComponent } from 'vue';
 import './Navigation.css';
 import { RouterLink } from 'vue-router';
-import {
-  GlobeAltIcon,
-  InformationCircleIcon,
-  HomeIcon,
-} from '@heroicons/vue/solid';
+import InfoIcon from '../../assets/icons/info-circle.svg';
+import HomeIcon from '../../assets/icons/home.svg';
+import MapMarkedAltIcon from '../../assets/icons/map-marked-alt.svg';
+
 import { routes } from '@/router';
 
 export default defineComponent({
@@ -16,9 +15,21 @@ export default defineComponent({
       <nav class="navigation">
         {routes.map((route) => (
           <RouterLink class="router-link" to={route.path}>
-            {'home' === route.name && <HomeIcon class="icon" />}
-            {'about' === route.name && <InformationCircleIcon class="icon" />}
-            {'map' === route.name && <GlobeAltIcon class="icon" />}
+            {'home' === route.name && (
+              <div class="icon">
+                <HomeIcon />
+              </div>
+            )}
+            {'about' === route.name && (
+              <div class="icon">
+                <InfoIcon />
+              </div>
+            )}
+            {'map' === route.name && (
+              <div class="icon">
+                <MapMarkedAltIcon />
+              </div>
+            )}
           </RouterLink>
         ))}
       </nav>
