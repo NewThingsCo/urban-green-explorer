6fdeec28fa2d9c00deacd1fa3aa530ab57c2a743
Sarah <robertz.sarah@gmail.com>
2022-05-01 21:25:53 +0300

    Fix: Hero and IndexPage test

---
+++ src/components/Hero/Hero.test.ts
@@ -0,0 +1,23 @@
+import { createTestingPinia } from '@pinia/testing';
+import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
+import Component from './Hero';
+import { i18n } from '@/utils';
+
+const wrapper = mount(Component, {
+  global: {
+    plugins: [createTestingPinia(), i18n],
+  },
+});
+
+describe(Component.name, () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+  it('renders the component', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+  it('has the correct amount of children', () => {
+    expect(wrapper.find('.hero').element.childNodes.length).toEqual(2);
+  });
+});
