57f337c7a5fab1b99641666f9b28126a2e788bbc
Sarah <robertz.sarah@gmail.com>
2022-05-20 00:04:33 +0300

    Style: Height & images on locations page

--- src/pages/LocationsPage/LocationsPage.tsx
+++ src/pages/LocationsPage/LocationsPage.tsx
@@ -2,6 +2,9 @@ import type { VNode } from 'vue';
 import { defineComponent } from 'vue';
 import { RouterLink } from 'vue-router';
 import ParklyImage from '@/assets/images/Parkly.jpeg';
+import AuroraImage from '@/assets/images/Aurora.jpeg';
+import LoviseholmImage from '@/assets/images/Loviseholm.jpeg';
+import TramImage from '@/assets/images/Tram.jpeg';
 import MapMarkerAlt from '@/assets/icons/map-marker-alt.svg?component';
 import AppFooter from '@/components/AppFooter';
 import AppHeader from '@/components/AppHeader';
@@ -12,9 +15,18 @@ import './LocationsPage.css';
 export default defineComponent({
   name: 'LocationsPage',
   setup() {
-    const imgUrl = new URL(ParklyImage, import.meta.url).href;
+    const parklyUrl = new URL(ParklyImage, import.meta.url).href;
+    const auroraUrl = new URL(AuroraImage, import.meta.url).href;
+    const loviseholmUrl = new URL(LoviseholmImage, import.meta.url).href;
+    const greenUrbanUrl = new URL(ParklyImage, import.meta.url).href;
+    const tramUrl = new URL(TramImage, import.meta.url).href;
+
     return {
-      imgUrl,
+      parklyUrl,
+      auroraUrl,
+      loviseholmUrl,
+      greenUrbanUrl,
+      tramUrl,
     };
   },
   render(): VNode {
@@ -31,8 +43,44 @@ export default defineComponent({
                     class="cursor-pointer mr-3"
                     href={`/location/${location.params}`}
                   >
-                    {' '}
-                    <img src={this.imgUrl} class="object-fill h-20 w-43" />
+                    {(() => {
+                      switch (location.params) {
+                        case 'parkly':
+                          return (
+                            <img
+                              src={this.parklyUrl}
+                              class="object-fill h-21 w-43"
+                            />
+                          );
+                        case 'aurora-block':
+                          return (
+                            <img
+                              src={this.auroraUrl}
+                              class="object-fill h-21 w-43"
+                            />
+                          );
+                        case 'loviseholm-park':
+                          return (
+                            <img
+                              src={this.loviseholmUrl}
+                              class="object-fill h-21 w-43"
+                            />
+                          );
+                        case 'green-urban-mapping':
+                          return (
+                            <div class="bg-gray-400 object-fill h-21 w-26" />
+                          );
+                        case 'green-tram-stop':
+                          return (
+                            <img
+                              src={this.tramUrl}
+                              class="object-fill h-21 w-43"
+                            />
+                          );
+                        default:
+                          return null;
+                      }
+                    })()}
                   </a>
                   <div class="flex flex-col w-full">
                     <h2>
