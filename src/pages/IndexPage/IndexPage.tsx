c646d3f3ea0371202c4477be0ba454568aa58b32
Sarah <robertz.sarah@gmail.com>
2022-05-20 09:58:39 +0300

    Fix: Logos

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,10 +1,10 @@
 import { defineComponent } from 'vue';
-import BGLogo from '@/assets/logos/bglogo.png';
-import EuLogo from '@/assets/logos/eu.png';
-import TallinnLogo from '@/assets/logos/tallinn.png';
-import SeiLogo from '@/assets/logos/sei.png';
-import HelsinkiLogo from '@/assets/logos/helsinki.png';
-import InterregLogo from '@/assets/logos/interreg.png';
+import BGLogo from '@/assets/logos/bglogo.png?url';
+import EULogo from '@/assets/logos/eu.png?url';
+import TallinnLogo from '@/assets/logos/tallinn.png?url';
+import SeiLogo from '@/assets/logos/sei.png?url';
+import HelsinkiLogo from '@/assets/logos/helsinki.png?url';
+import InterregLogo from '@/assets/logos/interreg.png?url';
 import AppFooter from '@/components/AppFooter';
 import AppHeader from '@/components/AppHeader';
 import AppMain from '@/components/AppMain';
@@ -13,22 +13,6 @@ import './Index.css';
 
 export default defineComponent({
   name: 'IndexPage',
-  setup() {
-    const bgLogo = new URL(BGLogo, import.meta.url).href;
-    const tallinnLogo = new URL(TallinnLogo, import.meta.url).href;
-    const helsinkiLogo = new URL(HelsinkiLogo, import.meta.url).href;
-    const seiLogo = new URL(SeiLogo, import.meta.url).href;
-    const euLogo = new URL(EuLogo, import.meta.url).href;
-    const interregLogo = new URL(InterregLogo, import.meta.url).href;
-    return {
-      bgLogo,
-      tallinnLogo,
-      helsinkiLogo,
-      seiLogo,
-      euLogo,
-      interregLogo,
-    };
-  },
   render() {
     return (
       <>
@@ -37,12 +21,12 @@ export default defineComponent({
           <Hero />
           <div class="index-wrapper">
             <div class="logos">
-              <img class="h-1/2" src={this.seiLogo} />
-              <img class="h-1/2" src={this.tallinnLogo} />
-              <img class="h-1/2" src={this.bgLogo} />
-              <img class="h-1/2" src={this.helsinkiLogo} />
-              <img class="h-1/2" src={this.euLogo} />
-              <img class="h-1/2" src={this.interregLogo} />
+              <img class="h-1/2" src={SeiLogo} />
+              <img class="h-1/2" src={TallinnLogo} />
+              <img class="h-1/2" src={BGLogo} />
+              <img class="h-1/2" src={HelsinkiLogo} />
+              <img class="h-1/2" src={EULogo} />
+              <img class="h-1/2" src={InterregLogo} />
             </div>
             <span class="text-sm text-gray-400">© 2022 B.Green project</span>
           </div>
