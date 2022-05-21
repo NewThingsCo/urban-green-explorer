589fb4adc74d90c42fc36088ac30fd2840518d12
Sarah <robertz.sarah@gmail.com>
2022-05-21 10:55:13 +0300

    Style: Clean up

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,4 +1,4 @@
-import type { PropType, VNode } from 'vue';
+import type { VNode } from 'vue';
 import { defineComponent } from 'vue';
 import { RouterLink, useRouter } from 'vue-router';
 import LocaleSwitcher from '../LocaleSwitcher';
