74bc19efe48c7be107b1e86307cb371e5067486d
Niko Pettersen <niko.pettersen@newthings.co>
2022-06-14 18:28:06 +0300

    Feat: Use router push for router links within popups

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,5 +1,5 @@
 import type { Location } from '@/types';
-import type { Layer, Map, TileLayer } from 'leaflet';
+import type { Layer, Map, PopupEvent, TileLayer } from 'leaflet';
 import {
   VNode,
   VNodeRef,
@@ -8,10 +8,11 @@ import {
   defineComponent,
   onMounted,
   ref,
+  onBeforeUnmount,
 } from 'vue';
 import leaflet from 'leaflet';
 import { useI18n } from 'vue-i18n';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import MapMarkerAltIcon from '@/assets/icons/map-marker-alt.svg?raw';
 import AppFooter from '@/components/AppFooter';
 import AppHeader from '@/components/AppHeader';
@@ -26,16 +27,17 @@ export default defineComponent({
   setup() {
     const { locale, t } = useI18n();
     const route = useRoute();
+    const router = useRouter();
     const mapInstance = ref<null | Map>(null);
     const layers = ref<null | TileLayer>(null);
     const map = ref<null | VNodeRef>(null);
 
-    /** Marker icon used in the map. */
+    /** Marker icon used for locations. */
     const markerIcon = leaflet.divIcon({
-      html: MapMarkerAltIcon,
       className: '',
-      iconSize: [24, 40],
+      html: MapMarkerAltIcon,
       iconAnchor: [12, 40],
+      iconSize: [24, 40],
     });
 
     /** Location slug. */
@@ -69,6 +71,54 @@ export default defineComponent({
       locations.map(createLocationMarker);
     }
 
+    /** Handles route changes based on the `href` value of a link. */
+    function handleRouteChange(event: Event): void {
+      event.preventDefault();
+      const href = (event.target as HTMLAnchorElement).href;
+      const path = new URL(href).pathname;
+      console.debug('Redirecting to:', path);
+      router.push({ path });
+    }
+
+    /** Binds event handlers to router links within popups. */
+    function bindPopupLinks(): void {
+      Array.from(
+        map.value.$el.querySelectorAll('.router-link') as HTMLCanvasElement[]
+      ).forEach(($link) => {
+        console.debug('Adding event listener to:', $link);
+        $link.addEventListener('click', handleRouteChange);
+      });
+    }
+
+    /** Removes event handlers from router links within popups. */
+    function resetPopupLinks(): void {
+      Array.from(
+        map.value.$el.querySelectorAll('.router-link') as HTMLCanvasElement[]
+      ).forEach(($link) => {
+        console.debug('Removing event listener from:', $link);
+        $link.removeEventListener('click', handleRouteChange);
+      });
+    }
+
+    /** Handles `popupclose` events. */
+    function handlePopupclose(event: PopupEvent): void {
+      resetPopupLinks();
+      console.debug('Popup closed:', event);
+    }
+
+    /** Handles `popupopen` events. */
+    function handlePopupopen(event: PopupEvent): void {
+      console.debug('Popup opened:', event);
+      bindPopupLinks();
+    }
+
+    /** Removes markers from the map. */
+    function removeMarker(layer: Layer): void {
+      if (!layer.hasEventListeners('click')) return;
+      console.debug('Removing marker:', layer);
+      layer.remove();
+    }
+
     /** Renders popup content. */
     function renderPopup(l: Location): string {
       return `<div class="flex flex-col items-center"><h2 class="text-sm">${t(
@@ -76,13 +126,6 @@ export default defineComponent({
       )}</h2><a class="router-link" href="/location/${l.slug}">${t(
         'moreInfo'
       )}</a></div>`;
-    }
-
-    /** Removes a layer from the map if it contains click events. */
-    function removeMarker(layer: Layer): void {
-      if (!layer.hasEventListeners('click')) return;
-      console.debug('Removing marker:', layer);
-      layer.remove();
     }
 
     /** Resets the map and re-renders popups. */
@@ -113,6 +156,10 @@ export default defineComponent({
       // Invalidate map on window resize
       resizeObserver.observe(map.value.$el);
 
+      // Handle popup events
+      mapInstance.value.addEventListener('popupclose', handlePopupclose);
+      mapInstance.value.addEventListener('popupopen', handlePopupopen);
+
       // Open pre-defined popup
       if (location) {
         console.debug('Showing popup for pre-defined location:', location);
@@ -124,11 +171,21 @@ export default defineComponent({
       }
     }
 
+    /** Removes event listeners from instance. */
+    function removeEventListeners(): void {
+      mapInstance.value?.removeEventListener('popupclose', handlePopupclose);
+      mapInstance.value?.removeEventListener('popupopen', handlePopupopen);
+      resetPopupLinks();
+    }
+
     // Watch for locale changes
     watch(locale, handleLocaleChange);
 
-    // Initialize map on load
+    // Initialize map on mount
     onMounted(initializeMap);
+
+    // Remove event listeners on unmount
+    onBeforeUnmount(removeEventListeners);
 
     return { map };
   },
