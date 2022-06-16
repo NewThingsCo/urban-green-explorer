c1dbfce5fafffd959593bcd4bc436259bc7114c6
Niko Pettersen <niko.pettersen@newthings.co>
2022-06-16 08:36:08 +0300

    Feat: Create component for displaying location images

---
+++ src/components/ImageList/ImageList.tsx
@@ -0,0 +1,30 @@
+import type { LocationImage } from '@/types';
+import type { PropType, VNode } from 'vue';
+import { defineComponent } from 'vue';
+import './ImageList.css';
+
+export default defineComponent({
+  name: 'ImageList',
+  props: {
+    images: {
+      default: () => [],
+      type: Array as PropType<LocationImage[]>,
+    },
+  },
+  render(): VNode {
+    return (
+      <ul aria-label={this.$t('locationImages')} class="image-list">
+        {this.images.map(({ alt, caption, src }) => (
+          <li class="item">
+            <figure class="container">
+              <img alt={this.$t(alt)} class="image" src={src} />
+              {caption && (
+                <figcaption class="caption">{this.$t(caption)}</figcaption>
+              )}
+            </figure>
+          </li>
+        ))}
+      </ul>
+    );
+  },
+});
---
+++ src/components/ImageList/index.ts
@@ -0,0 +1,3 @@
+import Component from './ImageList';
+
+export default Component;
