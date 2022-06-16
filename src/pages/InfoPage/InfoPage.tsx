524bfef110c44035d16d01e1213d4a4f4b1129c7
Niko Pettersen <niko.pettersen@newthings.co>
2022-06-16 09:19:48 +0300

    Feat: Add info page content

--- src/pages/InfoPage/InfoPage.tsx
+++ src/pages/InfoPage/InfoPage.tsx
@@ -3,6 +3,7 @@ import { defineComponent } from 'vue';
 import AppFooter from '@/components/AppFooter';
 import AppHeader from '@/components/AppHeader';
 import AppMain from '@/components/AppMain';
+import './InfoPage.css';
 
 export default defineComponent({
   name: 'InfoPage',
@@ -12,6 +13,32 @@ export default defineComponent({
         <AppHeader />
         <AppMain class="main-wrapper">
           <h1 class="title page-title">{this.$t('info.title')}</h1>
+          <h2 class="page-subtitle">{this.$t('info.subtitle')}</h2>
+          <dl class="list">
+            <dt class="list-title">{this.$t('producer')}</dt>
+            <dd>Forum Virium Helsinki</dd>
+            <dd>B.Green-project, 2020-2022</dd>
+          </dl>
+          <dl class="list">
+            <dt class="list-title">{this.$t('financier')}</dt>
+            <dd>European Union, Central Baltic Interreg program</dd>
+          </dl>
+          <dl class="list">
+            <dt class="list-title">{this.$t('bGreenContact')}</dt>
+            <dd>
+              <a href="https://bgreen-project.eu/contact/" target="_blank">
+                https://bgreen-project.eu/contact/
+              </a>
+            </dd>
+          </dl>
+          <dl class="list">
+            <dt class="list-title">{this.$t('implementation')}</dt>
+            <dd>
+              New Things Company Oy
+              <br />
+              <a href="https://newthings.co">https://newthings.co</a>
+            </dd>
+          </dl>
         </AppMain>
         <AppFooter />
       </>
