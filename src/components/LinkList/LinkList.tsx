10a9b24a8098afae2e07e78d8192d5e22c0ed30d
Niko Pettersen <niko.pettersen@newthings.co>
2022-06-16 08:35:41 +0300

    Refactor: Add support for Link List alerts and fix markup

--- src/components/LinkList/LinkList.tsx
+++ src/components/LinkList/LinkList.tsx
@@ -3,6 +3,7 @@ import type { LocationLink } from '@/types';
 import { defineComponent } from 'vue';
 import { RouterLink } from 'vue-router';
 import './LinkList.css';
+import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
   name: 'LinkList',
@@ -22,69 +23,94 @@ export default defineComponent({
       type: Function as PropType<(event: MouseEvent) => void>,
     },
   },
+  setup() {
+    const { t } = useI18n();
+
+    /** Generates a link's `href` value based on the correct type. */
+    function generateHrefValue(to: LocationLink['to']): string {
+      switch (true) {
+        // Translation
+        case 'string' === typeof to && to.startsWith('locations.'):
+          return t(to);
+        // Regular link
+        case 'string' === to:
+          return to;
+        // Router object
+        default:
+          return to.path?.toString();
+      }
+    }
+
+    /** Handles alerts from links. */
+    function handleAlert(event: Event, alert?: string): void {
+      if (!alert) return;
+      event.preventDefault();
+      window.alert(t(alert).toString());
+    }
+
+    return { generateHrefValue, handleAlert };
+  },
   render(): VNode {
     return (
-      <>
-        {this.links.length && (
-          <ul aria-label={this.$t('locationLinks')} class="link-list">
-            {this.links.map(
-              ({
-                iconLeft: IconLeft,
-                iconRight: IconRight,
-                title,
-                to,
-                type,
-              }) => {
-                switch (type) {
-                  case 'external':
-                    return (
-                      <li class="item">
-                        <a
-                          class="link"
-                          href={to.name?.toString()}
-                          rel="external"
-                          target="_blank"
-                        >
-                          {IconLeft && <IconLeft class="icon icon-left" />}
-                          <span class="title">{this.$t(title)}</span>
-                          {IconRight && <IconRight class="icon icon-right" />}
-                        </a>
-                      </li>
-                    );
-                  /** @todo */
-                  case 'panel':
-                    return (
-                      <li class="item">
-                        <a
-                          class="link"
-                          data-id={to.params?.id}
-                          href={`#panel-${to.params?.id}`}
-                          onClick={this.showPanel}
-                        >
-                          {IconLeft && <IconLeft class="icon icon-left" />}
-                          <span class="title">{this.$t(title)}</span>
-                          {IconRight && <IconRight class="icon icon-right" />}
-                        </a>
-                      </li>
-                    );
-                  case 'router-link':
-                    return (
-                      <li class="item">
-                        <RouterLink class="link" to={to}>
-                          {IconLeft && <IconLeft class="icon icon-left" />}
-                          <span class="title">{this.$t(title)}</span>
-                          {IconRight && <IconRight class="icon icon-right" />}
-                        </RouterLink>
-                      </li>
-                    );
-                  default:
-                    return null;
-                }
-              }
-            )}
-          </ul>
+      <ul aria-label={this.$t('locationLinks')} class="link-list">
+        {this.links.map(
+          ({
+            alert,
+            iconLeft: IconLeft,
+            iconRight: IconRight,
+            title,
+            to,
+            type,
+          }) => {
+            switch (type) {
+              case 'external':
+                return (
+                  <li class="item">
+                    <a
+                      class="link"
+                      href={this.generateHrefValue(to)}
+                      onClick={(event) => this.handleAlert(event, alert)}
+                      rel="external"
+                      target="_blank"
+                    >
+                      {IconLeft && <IconLeft class="icon icon-left" />}
+                      <span class="title">{this.$t(title)}</span>
+                      {IconRight && <IconRight class="icon icon-right" />}
+                    </a>
+                  </li>
+                );
+              /** @todo */
+              case 'panel':
+                return (
+                  <li class="item">
+                    <a
+                      class="link"
+                      data-id={to.params?.id}
+                      href={`#panel-${to.params?.id}`}
+                      onClick={this.showPanel}
+                    >
+                      {IconLeft && <IconLeft class="icon icon-left" />}
+                      <span class="title">{this.$t(title)}</span>
+                      {IconRight && <IconRight class="icon icon-right" />}
+                    </a>
+                  </li>
+                );
+              case 'router-link':
+                return (
+                  <li class="item">
+                    <RouterLink class="link" to={to}>
+                      {IconLeft && <IconLeft class="icon icon-left" />}
+                      <span class="title">{this.$t(title)}</span>
+                      {IconRight && <IconRight class="icon icon-right" />}
+                    </RouterLink>
+                  </li>
+                );
+              default:
+                return null;
+            }
+          }
         )}
-      </>
+      </ul>
     );
   },
 });
