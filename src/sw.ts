08d6b398ea63cd7519dba5234cb6f4903facbb4c
Niko Pettersen <niko.pettersen@newthings.co>
2022-07-05 12:47:47 +0300

    Feat: Create custom service worker (#124)
    * Deps: Add workbox related dependencies
    
    * Feat: Create custom service worker
    
    * Config: Add custom service worker to Vite Plugin PWA

---
+++ src/sw.ts
@@ -0,0 +1,20 @@
+import { clientsClaim } from 'workbox-core';
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+
+/**
+ * Custom Service Worker for Vite Plugin PWA
+ * @link https://vite-plugin-pwa.netlify.app/guide/inject-manifest.html#custom-service-worker
+ */
+declare let self: ServiceWorkerGlobalScope;
+
+cleanupOutdatedCaches();
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('message', (event) => {
+  if (event.data && 'SKIP_WAITING' === event.data.type) {
+    console.log('SKIP_WAITING detected.');
+    self.skipWaiting();
+  }
+});
